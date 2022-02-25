20ef5e1ad04a4b65f0ad60b8c07674b284c4c83d
기호 <kiho@gihoui-MacBookPro.local>
2022-02-25 15:10:04 +0900

    feat: TranparentImageCard 컴포넌트 추가

---
+++ real-front/src/examples/Cards/BlogCards/TransparentImageCard/index.js
@@ -0,0 +1,79 @@
+/**
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-pro-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+// @mui material components
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+function TransparentImageCard({ image, title }) {
+  const imageTemplate = (
+    <MKBox position="relative" borderRadius="lg">
+      <MKBox
+        component="img"
+        src={image}
+        alt={title}
+        borderRadius="lg"
+        shadow="md"
+        width="100%"
+        position="relative"
+        zIndex={1}
+      />
+      <MKBox
+        borderRadius="lg"
+        shadow="md"
+        width="100%"
+        height="100%"
+        position="absolute"
+        left={0}
+        top={0}
+        sx={{
+          backgroundImage: `url(${image})`,
+          transform: "scale(0.94)",
+          filter: "blur(12px)",
+          backgroundSize: "cover",
+        }}
+      />
+    </MKBox>
+  );
+
+  return (
+    <Card
+      sx={{
+        background: "transparent",
+        boxShadow: "none",
+        overflow: "visible",
+
+        "&:hover": {
+          cursor: "pointer",
+        },
+      }}
+    >
+      {imageTemplate}
+    </Card>
+  );
+}
+
+// Typechecking props for the TransparentImageCard
+TransparentImageCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default TransparentImageCard;
